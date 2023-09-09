27f8f253875d465ddf31c4f504ec2644dc43d8d7
georgetoloraia <gtoloraia19@gmail.com>
2023-09-09 20:43:03 +0400

    add calculation funqtion in .js

--- Front/scrypt.js
+++ Front/scrypt.js
@@ -1,3 +1,31 @@
-const kvadrati = document.getElementById("squareInput")
-const wertili = document.getElementById("circleInput")
+const kvadrati = document.getElementById("kvadrati");
+const wertili = document.getElementById("wertili");
+const calculateButton = document.getElementById("calculateButton");
+const resultDiv = document.getElementById("result");
 
+function CalculateResult() {
+    let Kvadrati1 = kvadrati.valueAsNumber
+    let Wertili1 = wertili.valueAsNumber
+    
+    {
+        if (Kvadrati1 <= 80) {
+            Wertili1 = Wertili1 * 15
+
+            resultDiv.textContent = `ფასი =  ${Wertili1}`;
+        }
+
+        if ( 80 < Kvadrati1 <= 160) {
+            Wertili1 = Wertili1 * 20
+
+            resultDiv.textContent = `ფასი = ${Wertili1}`
+        }
+
+        else { 
+            Wertili1 = Wertili1 * 25
+
+            resultDiv.textContent = `ფასი = ${Wertili1}`
+        }
+    }
+}
+
+calculateButton.addEventListener("click", CalculateResult);
